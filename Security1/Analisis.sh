3ad3fa5537630e155191f0198217186a0ed81634
PePoPiPu <alexsugimoto@outlook.com>
2024-02-11 00:59:31 +0100

    Avances en la página web

--- Security1/Analisis.sh
+++ Security1/Analisis.sh
@@ -6,7 +6,7 @@ OUTPUT_FILE="reporte_${MACHINE_IDENTIFIER}_$(date +'%Y%m%d_%H%M%S').txt"
 PERIOD="5 mins"
 
 # Ejecuta Snort, captura tráfico en tiempo real y guarda la salida en un archivo
-snort -c /ruta/a/snort.conf -q -l /ruta/de/salida -A ascii -v -i eno1 -K ascii -k none -N -l /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1 -b -B 512 &
+snort -c /etc/snort/snort.conf -q -l /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1 -v -i eno1 -K none -N -l /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1 -b -B 512 &
 
 # Espera el tiempo especificado para capturar el tráfico
 sleep 300
@@ -18,8 +18,8 @@ echo "Reporte de incidencias - $MACHINE_IDENTIFIER - $(date +'%Y-%m-%d %H:%M:%S'
 echo "Periodo de análisis: $PERIOD" >> $OUTPUT_FILE
 echo "----------------------------------------------" >> $OUTPUT_FILE
 # Genera el archivo del reporte:
-grep "Alerta" /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1/snortAlertas.log >> $OUTPUT_FILE
-grep "Alarma" /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1/snortAlarmas.log >> $OUTPUT_FILE
+grep "Alerta" /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1/snortAlertas.txt >> $OUTPUT_FILE
+grep "Alarma" /home/alexsugimoto/Documentos/DAM/Sistemas_Informáticos/2EV/ASAlex_Actividad_Evaluable2EV/Security1/snortAlarmas.txt >> $OUTPUT_FILE
 
 echo "Proceso completado. El reporte se ha guardado en: $OUTPUT_FILE"
 
